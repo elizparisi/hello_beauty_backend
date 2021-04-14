80aca833bbeaf117baaa3a05cda05208a42c9e27
Elizabeth Parisi <parisielizabeth@gmail.com>
2021-04-14 19:45:00 -0400

    add create action, strong params to products controller + add create to routes

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -2,6 +2,22 @@ class Api::V1::ProductsController < ApplicationController
 
   def index
     @products = Product.all
-    render json: @products 
+    render json: @products
+  end
+
+  def create
+    product = Product.new(product_params)
+
+    if product.save
+      render json: product, status: :accepted
+    else
+      render json: { errors: product.errors.full_messages }, status: :unprocessible_entity
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :category, :look_id)
   end
 end
