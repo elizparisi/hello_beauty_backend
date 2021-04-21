9cf63ba62caddb246a9a21b292aa394c347628c2
Elizabeth Parisi <parisielizabeth@gmail.com>
2021-04-21 01:09:18 -0400

    new seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,3 +11,11 @@
 # no_makeup = Look.create(name: "No Makeup")
 #
 # Product.create(name: "Fenty Beauty Blurring Skin Tint", category: "Face", look_id: work.id)
+
+skin = Category.create(name: "Skin")
+face = Category.create(name: "Face")
+eyes = Category.create(name: "Eyes")
+lips = Category.create(name: "Lips")
+hair = Category.create(name: "Hair")
+
+tester = Product.create(original_name: "Diorshow Mascara", original_price: 29.50, original_image_url: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fmedia1.popsugar-assets.com%2Ffiles%2Fthumbor%2FgPP6HUNTr3PG7HfN_v79Wo6IA8c%2Ffit-in%2F1024x1024%2Ffilters%3Aformat_auto-!!-%3Astrip_icc-!!-%2F2016%2F05%2F17%2F711%2Fn%2F1922153%2Fd629e70fa5ffd33e_s1689744-main-zoom%2Fi%2FDior-Diorshow-Mascara.jpg&f=1&nofb=1", dupe_name: "Neutrogena Healthy Volume Mascara", dupe_price: 7.64, dupe_image_url: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fntg-catalog.imgix.net%2Fproducts%2F6812436_carbon_black.jpg%3Fw%3D720%26h%3D866%26sfrm%3Djpg%26fit%3Dcrop&f=1&nofb=1", category_id: eyes.id)
